78f797301cb794af720e1ec19902f4649a61aaa4
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-08-01 12:13:29 +0200

    v0.1.0

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'miguelalarcos:react-modal',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'A simple modal for React Meteor.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/miguelalarcos/react-modal.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
